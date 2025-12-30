5656e818b90eadb417e188de01984861578f125b
tetsuyashibata0312 <shibata@chatplus.jp>
2025-12-30 23:22:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("ランチガチャ準備OK！");
+let lunchList = [];
+
+fetch("lunch_list.json")
+  .then(response => response.json())
+  .then(data => {
+    lunchList = data;
+  });
+
+document.getElementById("gachaBtn").addEventListener("click", () => {
+  if (lunchList.length === 0) return;
+
+  const shop = lunchList[Math.floor(Math.random() * lunchList.length)];
+  const comment =
+    shop.comments[Math.floor(Math.random() * shop.comments.length)];
+
+  document.getElementById("shop").textContent = shop.name;
+  document.getElementById("comment").textContent = comment;
+});
